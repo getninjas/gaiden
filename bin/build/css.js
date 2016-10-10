a88fe1bfc736e981b27f67a5a9e7d707cd5644a5
Eduardo Matos <eduardoj.matos@gmail.com>
2016-10-10 18:28:19 -0300

    Adding bin/build/css.js

---
+++ bin/build/css.js
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+
+const shelljs = require('shelljs');
+
+const sourceDir = "./src/sass/";
+const outputDir = "./dist/stylesheets";
+const compiledFile = `${outputDir}/ninja-gaiden.css`;
+const browserSupport = "> 0.1% in BR, not ie 8";
+const minifiedFile = "./dist/stylesheets/ninja-gaiden.min.css";
+
+const options = {
+  sass: ` --include-path ${sourceDir} \
+    --source-map true -r \
+    --output-style compressed \
+    -o ./dist/stylesheets`,
+  cssnano: `--replace true \
+    --autoprefixer.browsers "${browserSupport}" \
+    --safe \
+    --sourcemap`,
+  mqpacker: `-s`
+}
+
+shelljs.exec(`node-sass ${options.sass} ${sourceDir} ${compiledFile}`);
+shelljs.exec(`mqpacker ${options.mqpacker} ${compiledFile} ${compiledFile}`);
+shelljs.exec(`cssnano ${options.cssnano} ${compiledFile} ${minifiedFile}`);
