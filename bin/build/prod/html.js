c490d6ba59b955e9786a8681555fd8f499f6053d
Eduardo Matos <eduardoj.matos@gmail.com>
2016-10-28 15:52:03 -0200

    Removing html build scripts. We are using documentjs now

--- bin/build/prod/html.js
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env node
-
-const shelljs = require('shelljs');
-
-const sourceDir = "./src/html/";
-const outputDir = "./dist/";
-
-shelljs.exec(`rsync -av ${sourceDir}*.html ${outputDir}`);
