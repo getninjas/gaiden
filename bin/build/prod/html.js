e6b4791744cf3b108c680b088af53a2867c4e690
Eduardo Matos <eduardoj.matos@gmail.com>
2016-10-28 15:57:53 -0200

    Revert "Removing html build scripts. We are using documentjs now"
    This reverts commit c490d6ba59b955e9786a8681555fd8f499f6053d.

---
+++ bin/build/prod/html.js
@@ -0,0 +1,8 @@
+#! /usr/bin/env node
+
+const shelljs = require('shelljs');
+
+const sourceDir = "./src/html/";
+const outputDir = "./dist/";
+
+shelljs.exec(`rsync -av ${sourceDir}*.html ${outputDir}`);
