95eb86d3963f06dddefb414f4911aefef017e69e
Eduardo Matos <eduardoj.matos@gmail.com>
2017-04-29 10:25:20 -0300

    toString on stdout exec

--- bin/docs/prod/docs.js
+++ bin/docs/prod/docs.js
@@ -36,7 +36,7 @@ shell.ls('./docs/demo/*.html').forEach(function(file) {
 shell.mkdir('-p', './demo/gaiden-css');
 shell.cp('-Rf', './docs/demo/gaiden-css', './demo/gaiden-css');
 
-const gaidenLastTag = shell.exec('git describe').stdout;
+const gaidenLastTag = shell.exec('git describe').stdout.toString();
 
 shell.exec('git add . -A');
 shell.exec(`git commit -am '${timestamp}: Updating docs ${gaidenLastTag}`);
