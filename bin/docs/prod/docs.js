e664d00fac1b10abc6ab70ffb6bc995aa0e717ce
Eduardo Matos <eduardoj.matos@gmail.com>
2017-04-29 16:07:54 -0300

    Using mv instead cp

--- bin/docs/prod/docs.js
+++ bin/docs/prod/docs.js
@@ -35,7 +35,7 @@ filesToFixPath.forEach(function(file) {
   shell.sed('-i', '\/gaiden-css\/gaiden.css', '\/gaiden.css', `./docs/demo/${file}`);
 });
 
-shell.cp('-R', `${docsDir}/*`, './');
+shell.mv('-n', `${docsDir}/*`, './');
 
 shell.mkdir('-p', './demo/gaiden-css');
 shell.cp('-Rf', './docs/demo/gaiden-css', './demo/gaiden-css');
