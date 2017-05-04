d5fa655de0d2c73136b1183e33b68b509eed8910
Eduardo Matos <eduardoj.matos@gmail.com>
2017-05-04 16:23:05 -0300

    Adding new documentjs path

--- bin/docs/prod/docs.js
+++ bin/docs/prod/docs.js
@@ -19,8 +19,6 @@ class DocsGenerator {
     sourceDir = './src/scss',
     minifiedDocsFile = './docs/gaiden-css/ninja-demo.css'
   ) {
-    this.docFiles = [];
-    this.demoFiles = [];
 
     this.options = {
       sassSourceDocsDir: sassSourceDocsDir,
@@ -40,14 +38,13 @@ class DocsGenerator {
   start() {
     this.generateDocs();
     this.checkoutGh();
-    this.setupFiles();
+    this.setupFileNames();
     this.changePathNames();
-    this.fixStaticFiles();
     this.pushDocs();
   }
 
   generateDocs() {
-    sh.exec(`documentjs -f`);
+    sh.exec(`documentjs -f gh_pages`);
     sh.exec(`node-sass ${this.options.sourceDocsDir} ${this.options.sass} ${this.options.minifiedDocsFile}`);
   }
 
@@ -56,52 +53,12 @@ class DocsGenerator {
     sh.exec('git pull origin gh-pages --no-commit');
   }
 
-  setupFiles() {
+  setupFileNames() {
     sh.cp('-f', './dist/gaiden.min.css', './gaiden.css');
-
-    sh.find('./docs/demo/').forEach(function(file) {
-      validFile(this.demoFiles, file);
-    }.bind(this));
-
-    sh.find(`${this.options.docsDir}`).forEach(function(file) {
-      if (file.match(/(steal|html5shiv)/g)) return;
-
-      validFile(this.docFiles, file);
-    }.bind(this));
   }
 
   changePathNames() {
-    sh.sed('-i', '/gaiden-css/gaiden.css', '/gaiden/gaiden.css', this.demoFiles);
-
-    sh.sed('-i', '/demo/gaiden-css/', '/gaiden/demo/gaiden-css/', this.docFiles);
-    sh.sed('-i', '/docs/', '/', this.docFiles);
-    sh.sed('-i', '../docs/demo/', '/gaiden/demo/', this.docFiles);
-    sh.sed('-i', 'static/bundles/static.css', 'static.css', this.docFiles);
-    sh.sed('-i', 'static/bundles/static.js', 'static.js', this.docFiles);
-
-    if (sh.test('-e', './demo/')) {
-      sh.rm('-rf', './demo/*');
-    } else {
-      sh.mkdir('./demo/');
-    }
-
-    sh.rm('-f', './*.html');
-
-    sh.mv('-f', this.docFiles, './');
-    sh.mv('-f', this.demoFiles, './demo/');
-  }
-
-  fixStaticFiles() {
-    if (!sh.test('-e', './static')) {
-      sh.mkdir('./static');
-    }
-
-    sh.mv('-f', `${this.options.docsDir}/static/html5shiv.js`, './static/');
-    sh.mv('-f', `${this.options.docsDir}/static/steal.production.js`, './static/');
-    sh.mv('-f', `${this.options.docsDir}/static/bundles`, './static/');
-    sh.mv('-f', `${this.options.docsDir}/static/fonts`, './static/');
-    sh.mv('-f', `${this.options.docsDir}/static/img`, './static/');
-    sh.mv('-f', `${this.options.docsDir}/static/templates`, './static/');
+    sh.sed('-i', '/gaiden-css/gaiden.css', 'gaiden.css', './*.html');
   }
 
   pushDocs() {
