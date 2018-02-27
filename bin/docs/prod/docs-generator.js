8abe42c673ac1815f8eebab34bfdc4bb9c9a65af
Eduardo Matos <eduardoj.matos@gmail.com>
2018-02-27 11:52:54 -0300

    Deleting local branch gh-pages before pull on docs generator

--- bin/docs/prod/docs-generator.js
+++ bin/docs/prod/docs-generator.js
@@ -34,10 +34,11 @@ class DocsGenerator {
   checkoutGh() {
     sh.exec('COMMIT_DATE=$(date)');
     sh.exec('git commit -am "Dist commit preversion: "$COMMIT_DATE');
+    sh.exec('git branch -d gh-pages')
     sh.exec('git checkout -B gh-pages');
-    sh.exec('git merge master --no-commit');
-    sh.exec('git commit -m "Preversion from master: "$COMMIT_DATE');
     sh.exec('git pull origin gh-pages --no-commit');
+    sh.exec('git merge --no-commit -s ours master');
+    sh.exec('git commit -m "Preversion from master: "$COMMIT_DATE');
   }
 
   removeFileDocs() {
