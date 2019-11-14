cdbb15358b9178c7b6d54747840ec73f1ae25e3c
williamcosta <dev.williamcosta@gmail.com>
2019-11-14 10:37:29 -0300

    Dist commit preversion:

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 // Export a function. Accept the base config as the only param.
-module.exports = async ({ config, mode }) => {
+module.exports = async ({ config }) => {
   // `mode` has a value of 'DEVELOPMENT' or 'PRODUCTION'
   // You can change the configuration based on that.
   // 'PRODUCTION' is used when building the static version of storybook.
