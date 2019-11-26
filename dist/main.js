419fa754192bb60d4b4c278ea635e0b38d21023a
williamcosta <dev.williamcosta@gmail.com>
2019-11-26 14:54:34 -0300

    New Dist files

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,20 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	!function() {
+/******/ 		__webpack_require__.p = "";
+/******/ 	}();
+/******/ 	
+/************************************************************************/
+!function() {
+/* unused harmony default export */ var _unused_webpack_default_export = (__webpack_require__.p + "yellow/gaiden.css");
+}();
+!function() {
+/* unused harmony default export */ var _unused_webpack_default_export = (__webpack_require__.p + "default/gaiden.css");
+}();
 /******/ })()
 ;
