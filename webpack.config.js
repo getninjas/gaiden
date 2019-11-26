2b5447be42201b148a1e490ba8ba5920b4d8ce5e
williamcosta <dev.williamcosta@gmail.com>
2019-11-26 14:53:57 -0300

    Configure Webpack to build CSS to dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,50 @@
 const path = require('path');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-  entry: ['src/scss/yellow/gaiden.scss'],
+  entry: ['./src/scss/yellow/gaiden.scss', './src/scss/default/gaiden.scss'],
   output: {
     path: path.resolve(__dirname, 'dist'),
   },
   resolve: {
     extensions: ['.scss']
   },
+  optimization: {
+    minimizer: [new OptimizeCSSAssetsPlugin({})],
+  },
   module: {
-    rules: [{
-      test: /\.scss$/,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            name: 'css/[name].css',
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].css',
+              outputPath: (url, resourcePath, context) => {
+                if (/default/.test(resourcePath)) {
+                  return `default/${url}`;
+                }
+
+                return `yellow/${url}`;
+              }
+            },
+          },
+          {
+            loader: 'extract-loader'
+          },
+          {
+            loader: 'css-loader?-url'
+          },
+          {
+            loader: 'postcss-loader'
+          },
+          {
+            loader: 'sass-loader'
           }
-        },
-        {
-          loader: 'extract-loader'
-        },
-        {
-          loader: 'css-loader?-url'
-        },
-        {
-          loader: 'postcss-loader'
-        },
-        {
-          loader: 'sass-loader'
-        }
-      ]
-    }]
+        ]
+      },
+    ]
   }
 }
