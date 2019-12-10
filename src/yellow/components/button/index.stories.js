10a9d184d72e6b3ab4c84a8af2bf598445124ce5
William Costa <dev.williamcosta@gmail.com>
2019-12-10 17:37:04 -0300

    Create secondary, clear and ultimate buttons

--- src/yellow/components/button/index.stories.js
+++ src/yellow/components/button/index.stories.js
@@ -58,4 +58,92 @@ export const inverseLoading = () => `
   </div>
 `;
 
-export const secondary = () => '<button class="btn btn--secondary btn--small">Button Secondary</button>';
+export const secondary = () => `
+  <div class="docs">  
+    <button class="btn btn--secondary btn--small">Button Secondary</button>
+  </div>
+`;
+
+export const secondaryLarge = () => `
+  <div class="docs">  
+    <button class="btn btn--secondary btn--large">Button Secondary</button>
+  </div>
+`;
+
+export const secondaryDisabled = () => `
+  <div class="docs">  
+    <button class="btn btn--secondary btn--small" disabled="true">Button Secondary</button>
+  </div>
+`;
+
+export const secondaryLoading = () => `
+  <div class="docs">  
+    <button class="btn btn--secondary btn--small btn--loading">Button Secondary</button>
+  </div>
+`;
+
+export const secondaryLargeLoading = () => `
+  <div class="docs">  
+    <button class="btn btn--secondary btn--large btn--loading">Button Secondary</button>
+  </div>
+`;
+
+export const secondaryInverse = () => `
+  <div class="docs docs--inverse">  
+    <button class="btn btn--secondary btn--small btn--inverse">Button Secondary</button>
+  </div>
+`;
+
+export const secondaryInverseDisabled = () => `
+  <div class="docs docs--inverse">  
+    <button class="btn btn--secondary btn--small btn--inverse" disabled="true">Button Secondary</button>
+  </div>
+`;
+
+export const secondaryInverseLoading = () => `
+  <div class="docs docs--inverse">  
+    <button class="btn btn--secondary btn--small btn--inverse btn--loading">Button Secondary</button>
+  </div>
+`;
+
+export const clear = () => `
+  <div class="docs">  
+    <button class="btn btn--clear btn--small">Button Clear</button>
+  </div>
+`;
+
+export const clearLarge = () => `
+  <div class="docs">  
+    <button class="btn btn--clear btn--large">Button Clear</button>
+  </div>
+`;
+
+export const clearDisabled = () => `
+  <div class="docs">  
+    <button class="btn btn--clear btn--small" disabled="true">Button Clear</button>
+  </div>
+`;
+
+export const ultimate = () => `
+  <div class="docs">  
+    <button class="btn btn--ultimate btn--small">Button Ultimate</button>
+  </div>
+`;
+
+export const ultimateLarge = () => `
+  <div class="docs">  
+    <button class="btn btn--ultimate btn--large">Button Ultimate</button>
+  </div>
+`;
+
+export const ultimateDisabled = () => `
+  <div class="docs">  
+    <button class="btn btn--ultimate btn--small" disabled="true">Button Ultimate</button>
+  </div>
+`;
+
+export const ultimateLoading = () => `
+  <div class="docs">  
+    <button class="btn btn--ultimate btn--small btn--loading">Button Ultimate</button>
+  </div>
+`;
