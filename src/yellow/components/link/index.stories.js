48f3432b2f34974194b176d4904cdd0fc7da426a
williamcosta <dev.williamcosta@gmail.com>
2019-12-19 08:35:25 -0300

    Adjustment documentation component

--- src/yellow/components/link/index.stories.js
+++ src/yellow/components/link/index.stories.js
@@ -14,17 +14,29 @@ export const allLinks = () => `
   <div class="docs docs--inverse">
     <a class="link link--inverse">Link</h1>
   </div>
-`;
-
-export const linkLarge = () => `
   <div class="docs">
     <a class="link link--large">Link</h1>
+  </div>
+  <div class="docs docs--inverse">
+    <a class="link link--large link--inverse">Link</h1>
+  </div>
+`;
+
+export const link = () => `
+  <div class="docs">
+    <a class="link">Link</h1>
   </div>
 `;
 
 export const linkInverse = () => `
   <div class="docs docs--inverse">
     <a class="link link--inverse">Link</h1>
+  </div>
+`;
+
+export const linkLarge = () => `
+  <div class="docs">
+    <a class="link link--large">Link</h1>
   </div>
 `;
 
