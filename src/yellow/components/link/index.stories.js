36d878cdffa0d4d06fdbac8d4d4b900a5e6032ed
williamcosta <dev.williamcosta@gmail.com>
2019-12-19 08:33:57 -0300

    Create Link Component

---
+++ src/yellow/components/link/index.stories.js
@@ -0,0 +1,35 @@
+export default {
+  title: 'Components | Link',
+  parameters: {
+    componentSubtitle: 'Link is used in link to others pages.',
+  },
+};
+
+import './_index.scss';
+
+export const allLinks = () => `
+  <div class="docs">
+    <a class="link">Link</h1>
+  </div>
+  <div class="docs docs--inverse">
+    <a class="link link--inverse">Link</h1>
+  </div>
+`;
+
+export const linkLarge = () => `
+  <div class="docs">
+    <a class="link link--large">Link</h1>
+  </div>
+`;
+
+export const linkInverse = () => `
+  <div class="docs docs--inverse">
+    <a class="link link--inverse">Link</h1>
+  </div>
+`;
+
+export const linkLargeInverse = () => `
+  <div class="docs docs--inverse">
+    <a class="link link--large link--inverse">Link</h1>
+  </div>
+`;
